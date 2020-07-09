8a884b51bac7453ee9a1a1ec561a08b9f2d3b14b
Sébastien Grivel <s.grivel@ecole-ipssi.net>
2020-07-09 16:15:43 +0200

    Ajout fonction retirant les pays sans capitale

--- quizCountries/playground.py
+++ quizCountries/playground.py
@@ -14,6 +14,18 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+def getFourDifferentsCountriesWithCapitale(nbre_pays, rq_pays):
+	pays = rq_pays.json()
+	while True:
+		key_reponse, key_choix_2, key_choix_3, key_choix_4 = getFourDifferentKeys(nbre_pays)
+		if "" not in (pays[key_reponse]["capital"], pays[key_choix_2]["capital"], pays[key_choix_3]["capital"], pays[key_choix_4]["capital"]):
+			break
+		print(f"\t Pays/capitales 1 : {pays[key_reponse]['name']}/{pays[key_reponse]['capital']}\n \
+				Pays/capitales 2 : {pays[key_choix_2]['name']}/{pays[key_choix_2]['capital']}\n \
+				Pays/capitales 3 : {pays[key_choix_3]['name']}/{pays[key_choix_3]['capital']}\n \
+				Pays/capitales 4 : {pays[key_choix_4]['name']}/{pays[key_choix_4]['capital']}")
+	return key_reponse, key_choix_2, key_choix_3, key_choix_4
+
 def getFourDifferentKeys(nbre_pays):
 	key_response = randrange(1, nbre_pays)
 	keysTab = [key_response]
@@ -37,8 +49,16 @@ def getFourDifferentKeys(nbre_pays):
 url_total_pays = "https://restcountries.eu/rest/v2/all"
 rq_pays = requests.get(url_total_pays)
 nbre_pays = len(rq_pays.json())
-key_reponse, key_choix_2, key_choix_3, key_choix_4 = getFourDifferentKeys(nbre_pays)
-print(f"\t key_response : {key_reponse}\n \
-        key_choix_2 : {key_choix_2}\n \
-        key_choix_3 : {key_choix_3}\n \
-        key_choix_4 : {key_choix_4}\n")
+key_reponse, key_choix_2, key_choix_3, key_choix_4 = getFourDifferentsCountriesWithCapitale(nbre_pays, rq_pays)
+# pays_reponse = rq_pays.json()[key_reponse]
+#pays_reponse["capitale"]
+
+# teste le fait que l'application nous ressort 4 pays différents
+# url_total_pays = "https://restcountries.eu/rest/v2/all"
+# rq_pays = requests.get(url_total_pays)
+# nbre_pays = len(rq_pays.json())
+# key_reponse, key_choix_2, key_choix_3, key_choix_4 = getFourDifferentKeys(nbre_pays)
+# print(f"\t key_response : {key_reponse}\n \
+#         key_choix_2 : {key_choix_2}\n \
+#         key_choix_3 : {key_choix_3}\n \
+#         key_choix_4 : {key_choix_4}")
